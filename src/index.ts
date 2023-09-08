18b22d8285ae846afcb4a26475fbba54e255884b
Bishal Bhandari <psybb7@exmail.nottingham.ac.uk>
2023-09-08 15:56:04 +0100

    setting up quotes endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,26 @@
 import express, { Application } from "express";
+import { connectDB } from "./database/db";
+import * as dotenv from "dotenv";
+import {router as quotesRouter} from "./routes/quotes";
+dotenv.config({ path: "./.env" });
+let {PORT} = process.env;
 
-const PORT = 8000;
+connectDB();
 
 const app: Application = express();
+// Set the API to handle JSON
+app.use(express.json());
 
-app.get("/quotes", async (_req, res) => {
-  res.send({[
-    {
-      "quote": "The best is yet to come!",
-      "author": "Unknown",
-      "category": "Optimism"
-    },
-    {
-      "quote": "We must accept finite disappointment, but never lose infinite hope.",
-      "author": "Martin Luther King Jr.",
-      "category": "Hope"
-    },
-    {
-      "quote": "Believe you can and you're halfway there.",
-      "author": "Theodore Roosevelt",
-      "category": "Belief"
-    },
-    {
-      "quote": "In the middle of every difficulty lies opportunity.",
-      "author": "Albert Einstein",
-      "category": "Opportunity"
-    },
-    {
-      "quote": "The future belongs to those who believe in the beauty of their dreams.",
-      "author": "Eleanor Roosevelt",
-      "category": "Dreams"
-    },
-    {
-      "quote": "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-      "author": "Winston Churchill",
-      "category": "Perseverance"
-    },
-    {
-      "quote": "It does not matter how slowly you go as long as you do not stop.",
-      "author": "Confucius",
-      "category": "Progress"
-    },
-    {
-      "quote": "The only way to do great work is to love what you do.",
-      "author": "Steve Jobs",
-      "category": "Passion"
-    },
-    {
-      "quote": "The harder the conflict, the greater the triumph.",
-      "author": "George Washington",
-      "category": "Challenge"
-    },
-    {
-      "quote": "The journey of a thousand miles begins with one step.",
-      "author": "Lao Tzu",
-      "category": "Start"
-    }
-  ]
-  });
-});
+// Enable Cross-Origin Resource Sharing
+const cors = require("cors");
+app.use(cors({origin: "*"}));
+
+
+app.use(express.urlencoded({ extended: false }));
+
+// Add routers for endpoints
+app.use("/api/quotes", quotesRouter);
+// app.use("/api/device", deviceTokenRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
