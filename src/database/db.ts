2d6c78ce8a5c188ab1a3ee5b44f88e9b85abb3fa
Bishal Bhandari <psybb7@exmail.nottingham.ac.uk>
2023-09-09 23:21:37 +0100

    added endpoint for device

--- src/database/db.ts
+++ src/database/db.ts
@@ -15,9 +15,13 @@ export const connectDB = async () => {
         "MONGO_<IP|USER|PASS> cannot be null - please specify in environment variable or file"
       );
     }
-
+    console.log("Connecting to MongoDB");
+    console.log(`MONGO_IP: ${MONGO_IP}`);
+    console.log(`MONGO_USER: ${MONGO_USER}`);
+    console.log(`MONGO_PASS: ${MONGO_PASS}`);
     // Initialise connection to MongoDB
     const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:27017/AffApp?authSource=admin`;
+    console.log(`MONGO_URI: ${MONGO_URI}`);
     const conn = await mongoose.connect(MONGO_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Application } from "express";
 import { connectDB } from "./database/db";
 import * as dotenv from "dotenv";
 import {router as quotesRouter} from "./routes/quotes";
+import {router as deviceRouter} from "./routes/tokens";
+
 dotenv.config({ path: "./.env" });
 let {PORT} = process.env;
 
@@ -20,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Add routers for endpoints
 app.use("/api/quotes", quotesRouter);
-// app.use("/api/device", deviceTokenRouter);
+app.use("/api/device", deviceRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
